98a15cc0c82e77a2cc247377fc1f1f33d15e60fd
Techie_BEee <147028563+Greatsuoul@users.noreply.github.com>
2024-12-06 15:06:56 +0530

    Add files via upload

--- userspace/main.c
+++ userspace/main.c
@@ -15,7 +15,7 @@
 dev_t dev = 0;
 static struct cdev file_cdev;
 static char *kernel_buffer;
-static struct class *dev_class; 
+static struct class *dev_class;
 
 static int FILE_OPEN(struct inode *inode, struct file *file);
 static int FILE_RELEASE(struct inode *inode, struct file *file);
@@ -30,18 +30,22 @@ static struct file_operations fops = {
     .release = FILE_RELEASE,
 };
 
-static int FILE_OPEN(struct inode *inode, struct file *file) {
+static int FILE_OPEN(struct inode *inode, struct file *file)
+{
     pr_info("driver open successfully\n");
     return 0;
 }
 
-static int FILE_RELEASE(struct inode *inode, struct file *file) {
+static int FILE_RELEASE(struct inode *inode, struct file *file)
+{
     pr_info("driver release successfully\n");
     return 0;
 }
 
-static ssize_t FILE_READ(struct file *file, char __user *buff, size_t len, loff_t *off) {
-    if (copy_to_user(buff, kernel_buffer, MEM_SIZE)) {
+static ssize_t FILE_READ(struct file *file, char __user *buff, size_t len, loff_t *off)
+{
+    if (copy_to_user(buff, kernel_buffer, MEM_SIZE))
+    {
         pr_err("Error in data read!!!\n");
         return -1;
     }
@@ -49,8 +53,10 @@ static ssize_t FILE_READ(struct file *file, char __user *buff, size_t len, loff_
     return MEM_SIZE;
 }
 
-static ssize_t FILE_WRITE(struct file *file, const char __user *buff, size_t len, loff_t *off) {
-    if (copy_from_user(kernel_buffer, buff, len)) {
+static ssize_t FILE_WRITE(struct file *file, const char __user *buff, size_t len, loff_t *off)
+{
+    if (copy_from_user(kernel_buffer, buff, len))
+    {
         pr_err("Error in data write!!!\n");
         return -EFAULT;
     }
@@ -58,9 +64,10 @@ static ssize_t FILE_WRITE(struct file *file, const char __user *buff, size_t len
     return len;
 }
 
-
-static int __init char1_init(void) {
-    if (alloc_chrdev_region(&dev, 0, 1, "character_device") < 0) {
+static int __init char1_init(void)
+{
+    if (alloc_chrdev_region(&dev, 0, 1, "character_device") < 0)
+    {
         printk(KERN_INFO "Character device allocation failed\n");
         return -1;
     }
@@ -68,20 +75,23 @@ static int __init char1_init(void) {
 
     cdev_init(&file_cdev, &fops);
 
-    if (cdev_add(&file_cdev, dev, 1) < 0) {
+    if (cdev_add(&file_cdev, dev, 1) < 0)
+    {
         unregister_chrdev_region(dev, 1);
         printk(KERN_INFO "Failed to add character device\n");
         return -1;
     }
 
-    dev_class = class_create(THIS_MODULE, "Driven_class");
-    if (IS_ERR(dev_class)) {
+    dev_class = class_create("Driven_class");
+    if (IS_ERR(dev_class))
+    {
         pr_info("Cannot Create Struct class\n");
         unregister_chrdev_region(dev, 1);
         return -1;
     }
 
-    if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "Driven_class"))) {
+    if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "Driven_class")))
+    {
         pr_info("Cannot Create Device\n");
         class_destroy(dev_class);
         unregister_chrdev_region(dev, 1);
@@ -89,7 +99,8 @@ static int __init char1_init(void) {
     }
 
     kernel_buffer = kmalloc(MEM_SIZE, GFP_KERNEL);
-    if (!kernel_buffer) {
+    if (!kernel_buffer)
+    {
         pr_info("Cannot allocate memory\n");
         class_destroy(dev_class);
         unregister_chrdev_region(dev, 1);
@@ -101,8 +112,8 @@ static int __init char1_init(void) {
     return 0;
 }
 
-
-static void __exit char1_exit(void) {
+static void __exit char1_exit(void)
+{
     kfree(kernel_buffer);
     class_destroy(dev_class);
     device_destroy(dev_class, dev);
